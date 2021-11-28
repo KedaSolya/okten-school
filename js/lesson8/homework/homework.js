d41e59b120515ce1976d2b7e012ed1515793253d
Keda.Solya <solya.keda@gmail.com>
2021-11-28 20:17:10 +0200

    js lesson 8 hw

--- js/lesson8/homework/homework.js
+++ js/lesson8/homework/homework.js
@@ -1,34 +1,34 @@
 // Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
 
-// -- отримує текст з параграфа з id "content"
+// - отримує текст з параграфа з id "content"
 // const content = document.getElementById('content').innerText
 
-// -- отримує текст з блоку з id "rules"
+// - отримує текст з блоку з id "rules"
 // const rules = document.getElementById('rules').innerText
 
-// -- замініть текст параграфа з id 'content' на будь-який інший
+// - замініть текст параграфа з id 'content' на будь-який інший
 // const contentText = document.getElementById('content')
 // contentText.innerText = 'Hello! It\'s changed text.'
 
-// -- замініть текст параграфа з id 'rules' на будь-який інший
+// - замініть текст параграфа з id 'rules' на будь-який інший
 // const rulesText = document.getElementById('rules')
 // rulesText.innerText = 'Hello! It\'s changed text.'
 
-// -- змініть кожному елементу колір фону на червоний
+// - змініть кожному елементу колір фону на червоний
 // const allElements = document.getElementsByTagName('body')
 // for (const allElement of allElements)
 //     allElement.style.backgroundColor = 'red'
 
-// -- змініть кожному елементу колір тексту на синій
+// - змініть кожному елементу колір тексту на синій
 // const allElements = document.getElementsByTagName('body')
 // for (const allElement of allElements)
 //     allElement.style.color = 'blue'
 
-// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+// - отримати весь список класів елемента з id=rules і вивести їх в console.log
 // const listClass = document.getElementById('rules')
 // console.log(listClass.classList)
 
-// -- поміняти колір тексту у всіх елементів fc_rules на червоний
+// - поміняти колір тексту у всіх елементів fc_rules на червоний
 // const fcRules = document.getElementsByClassName('fc_rules')
 // for (const fcRule of fcRules)
 //     fcRule.style.color = 'red'
